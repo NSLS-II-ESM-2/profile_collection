50e951c2b4367365461eb40a48f2cd1851680285
Thomas Hopkins <thopkins1@bnl.gov>
2025-05-06 09:42:11 -0400

    Add back publishing documents with kafka

--- startup/00-base.py
+++ startup/00-base.py
@@ -3,4 +3,10 @@ from ophyd.signal import EpicsSignalBase
 
 
 EpicsSignalBase.set_defaults(connection_timeout=10)
-nslsii.configure_base(get_ipython().user_ns, 'xpeem', redis_url="info.esm.nsls2.bnl.gov", redis_prefix="xpeem-")
+nslsii.configure_base(
+    get_ipython().user_ns, 
+    'xpeem',
+    publish_documents_with_kafka=True,
+    redis_url="info.esm.nsls2.bnl.gov",
+    redis_prefix="xpeem-"
+)
